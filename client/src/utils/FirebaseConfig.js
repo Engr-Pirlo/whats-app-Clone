7c535b57a6065c6fffcf6cd999b4b90ef0209c51
Omar Jibirl Abdullkadir <129175570+Engr-Pirlo@users.noreply.github.com>
2023-09-12 17:42:46 +0300

    configation firebase Auth

---
+++ client/src/utils/FirebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app"
+import {getAuth} from 'firebase/app'
+const firebaseConfig = {
+    apiKey: "AIzaSyAtpjrr-lIGrrL_hLXmfMO6V1SRH2D9q2Y",
+    authDomain: "what-sapp-clone-14f2c.firebaseapp.com",
+    projectId: "what-sapp-clone-14f2c",
+    storageBucket: "what-sapp-clone-14f2c.appspot.com",
+    messagingSenderId: "966773385358",
+    appId: "1:966773385358:web:6ed3558e4a7dcd8b1941df",
+    measurementId: "G-VB24E92HRP"
+  };
+
+
+const app  = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app)
+ 
