f957c7c6cef2362cb464fa3ae211dd43cf0b34a4
Omar Jibirl Abdullkadir <129175570+Engr-Pirlo@users.noreply.github.com>
2023-09-13 20:44:58 +0300

    login desing

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 function login() {
-  return <div>login</div>;
+  return <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
+    <div className="flex items-center justify-center gap-2 text-white">
+      <Image src="/whatsapp.git" alt="whatsapp" height={300} width={300} />
+
+    </div>
+  </div>;
 }
 
 export default login;
