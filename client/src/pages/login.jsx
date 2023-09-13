709ceb2dc5281d88522a66d5de5911c24f5f67af
Omar Jibirl Abdullkadir <129175570+Engr-Pirlo@users.noreply.github.com>
2023-09-13 22:57:06 +0300

    fixed image Error

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function login() {
   return <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
     <div className="flex items-center justify-center gap-2 text-white">
-      <Image src="/whatsapp.git" alt="whatsapp" height={300} width={300} />
+      <Image src="/whatsapp.gif" alt="whatsapp" height={300} width={300} />
 
     </div>
   </div>;
