30576c40cd8e1bd14297c4da63d1689985b23a23
Omar Jibirl Abdullkadir <129175570+Engr-Pirlo@users.noreply.github.com>
2023-09-16 09:39:48 +0300

    login form prepering

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,5 +1,20 @@
+import getPrismaInstance from "../utils/PrismaClient.js"
+
 export const checkUSer = async(req,res, next) =>{
     try{
+        const {email} = req.body
+        if(!email){
+            return res.json({msg: "Email is Required", status: false})
+        }
+        const prisma = getPrismaInstance();
+        const user = await prisma.user.findUnique({where: {email}})
+
+        if(!user){
+            return res.json({msg: "User not Found", status: false})
+        }
+        else{
+            return res.json({msg: "user Found" , status: true})
+        }
 
     }catch(error){
         next(error)
