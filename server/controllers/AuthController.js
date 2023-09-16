8426dbe5ed584c1703122768449c1ddbbb8ff186
Omar Jibirl Abdullkadir <129175570+Engr-Pirlo@users.noreply.github.com>
2023-09-16 09:54:21 +0300

    fixed confilc git

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-import getPrismaInstance from "../utils/PrismaClient.js"
+import getPrismaInstance from "../utils/PrismaClient"
 
-export const checkUSer = async(req,res, next) =>{
+export const checkUser = async (req, res, next) =>{
     try{
         const {email} = req.body
         if(!email){
@@ -9,32 +8,16 @@ export const checkUSer = async(req,res, next) =>{
         }
         const prisma = getPrismaInstance();
         const user = await prisma.user.findUnique({where: {email}})
-
         if(!user){
-            return res.json({msg: "User not Found", status: false})
+            return res.json({msg: "user not Found" , status: false})
         }
         else{
-            return res.json({msg: "user Found" , status: true})
-=======
-export const checkUser = async(req,res, next) =>{
-    try{
-        const {email} = req.body
-        if(!email){
-            return res.json({msg: "Email is required." , status: false})
+            return res.json({msg: "user Found", status: true})
         }
 
-        const prisma = getPrismaInstance()
-        const user = await prisma.user.findUnique({where:{email}})
-        console.log(user)
-        if(!user){
-            return res.json({msg: "User is Not Found", status: false})
-        }
-        else{
-            res.json({msg: "User Founded" , status: true})
->>>>>>> 8e15fac81b636009973baf0ea543ef81ed92866b
-        }
 
-    }catch(error){
-        next(error)
+    }catch(err){
+        next(err)
     }
 }
+
--- server/utils/PrismaClient.js
+++ server/utils/PrismaClient.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
+
 import {PrismaClient} from '@prisma/client'
 let prismaInstance =null;
 
 function getPrismaInstance(){
     if(!prismaInstance){
         prismaInstance = new PrismaClient()
-=======
-import { PrismaClient } from "@prisma/client.js";
-
-let prismaInstance = null;
-
-function getInstance(){
-    if(!prismaInstance){
-        prismaInstance = new PrismaClient
->>>>>>> 8e15fac81b636009973baf0ea543ef81ed92866b
     }
     return prismaInstance
 }
 
-<<<<<<< HEAD
+
 export default getPrismaInstance
-=======
-export default getInstance
->>>>>>> 8e15fac81b636009973baf0ea543ef81ed92866b
+
+
+
