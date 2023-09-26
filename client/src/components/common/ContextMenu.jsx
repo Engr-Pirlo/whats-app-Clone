e91e934d341bcfa79bdf8c9297144c535643e6d3
Omar Jibirl Abdullkadir <129175570+Engr-Pirlo@users.noreply.github.com>
2023-09-26 10:10:35 +0300

    uploadig avatar image

--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,30 +1,54 @@
-import React, { useRef } from "react";
-
-function ContextMenu({options, coordinates, contextMenu , SetContextMenu}) {
-  console.log(options)
+import React, { useEffect, useRef } from "react";
+function ContextMenu({ options, coordinates, contextMenu, setContextMenu }) {
+  console.log(options);
   const contextMenuRef = useRef(null);
-  const handleClick = (e, callback) =>{
-         
-  }
+  useEffect(() => {
+    const handleOutSideClick = (even) => {
+      if (even.target.id !== "context-opener") {
+        if (
+          contextMenuRef.current &&
+          !contextMenuRef.current.contains(even.target)
+        ) {
+          setContextMenu(false);
+        }
+      }
+    };
+    
+    document.addEventListener('click', handleOutSideClick);
+    return () =>{
+      document.removeEventListener('click' , handleOutSideClick);
+    }
 
-  return <div
-   className={`bg-dropdown-background fixed py-2 z-[100] top-[${coordinates.y}] left-[${coordinates.x}]`}
-   ref={contextMenuRef}
-  
-  >
-    <ul>
-      {options.map(({name , callback})=>(
-        <li key={name} onClick={(e)=>handleClick(e, callback)}><span className="text-white">{name}</span></li>
-      ))}
-      
-    </ul>
+  }, []);
+  const handleClick = (e, callback) => {
+    e.stopPropagation();
+    setContextMenu(false);
+    callback();
+  };
 
-  ContextMenu
-  </div>
-    
-    
-    
-   
+  return (
+    <div
+      className={`bg-dropdown-background fixed py-2 z-[100]`}
+      ref={contextMenuRef}
+      style={{
+        top: coordinates.y,
+        left: coordinates.x,
+      }}
+    >
+      <ul>
+        {options.map(({ name, callback }) => (
+          <li
+            key={name}
+            onClick={(e) => handleClick(e, callback)}
+            className="px-5 y-2 cursor-pointer hover:bg-background-default-hover"
+          >
+            <span className="text-white">{name}</span>
+          </li>
+        ))}
+      </ul>
+      ContextMenu
+    </div>
+  );
 }
 
 export default ContextMenu;
